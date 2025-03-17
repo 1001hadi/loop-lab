62bc898492db6d747fd9e4ef0d23777b878723bf
hadi.jaf <hadi.jaf.1001@gmail.com>
2025-03-17 15:15:50 -0600

    prime time solved

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
+/////////////////////
 // 1st Part: FizzBuzz
+/////////////////////
 
-let numbers = 100;
+// const numbers = 100;
 
 // for (let i = 1; i <= numbers; i++) {
 //   if (i % 3 === 0 && i % 5 === 0) {
@@ -16,18 +18,52 @@ let numbers = 100;
 
 // // FizzBuzz with switch
 
-for (let i = 1; i <= numbers; i++) {
-  switch (true) {
-    case i % 3 === 0 && i % 5 === 0:
-      console.log("FizBuz");
+// for (let i = 1; i <= numbers; i++) {
+//   switch (true) {
+//     case i % 3 === 0 && i % 5 === 0:
+//       console.log("FizBuz");
+//       break;
+//     case i % 3 === 0:
+//       console.log("Fizz");
+//       break;
+//     case i % 5 === 0:
+//       console.log("Buzz");
+//       break;
+//     default:
+//       console.log(i);
+//   }
+// }
+
+//////////////////////
+// 2nd Part: Prime Time
+/////////////////////
+
+const n = 10;
+
+for (let i = 1; i <= n; i++) {
+  let currNum = i;
+  //   console.log(currNum);
+  while (true) {
+    currNum++;
+    let primeNum = true;
+
+    if (currNum <= 1) {
+      primeNum = false;
+    } else if (currNum <= 3) {
+      primeNum = true;
+    } else if (currNum % 2 === 0 || currNum % 3 === 0) {
+      primeNum = false;
+    } else { // Got hint from stack Overflow for this part.
+      for (let j = 5; j * j <= currNum; j += 6) {
+        if (currNum % j === 0 || (currNum % i) + 2 === 0) {
+          primeNum = false;
+          break;
+        }
+      }
+    }
+    if (primeNum) {
+      console.log(`${i}\n\ next Prime number is: ${currNum}`);
       break;
-    case i % 3 === 0:
-      console.log("Fizz");
-      break;
-    case i % 5 === 0:
-      console.log("Buzz");
-      break;
-    default:
-      console.log(i);
+    }
   }
 }
