5540b6575227625a0a9fdd1aab13ccc9458bbce8
hadi.jaf <hadi.jaf.1001@gmail.com>
2025-03-17 14:23:17 -0600

    switch statment fizzbuzz created

--- app.js
+++ app.js
@@ -2,14 +2,32 @@
 
 let numbers = 100;
 
+// for (let i = 1; i <= numbers; i++) {
+//   if (i % 3 === 0 && i % 5 === 0) {
+//     console.log("FizBuz");
+//   } else if (i % 3 === 0) {
+//     console.log("Fizz");
+//   } else if (i % 5 === 0) {
+//     console.log("Buzz");
+//   } else {
+//     console.log(i);
+//   }
+// }
+
+// // FizzBuzz with switch
+
 for (let i = 1; i <= numbers; i++) {
-  if (i % 3 === 0 && i % 5 === 0) {
-    console.log("FizBuz");
-  } else if (i % 3 === 0) {
-    console.log("Fizz");
-  } else if (i % 5 === 0) {
-    console.log("Buzz");
-  } else {
-    console.log(i);
+  switch (true) {
+    case i % 3 === 0 && i % 5 === 0:
+      console.log("FizBuz");
+      break;
+    case i % 3 === 0:
+      console.log("Fizz");
+      break;
+    case i % 5 === 0:
+      console.log("Buzz");
+      break;
+    default:
+      console.log(i);
   }
 }
