f1aff6e10801f6e5841430901dfe3855b8bb7376
hadi.jaf <hadi.jaf.1001@gmail.com>
2025-03-17 16:36:23 -0600

    try catch method added to 3rd problem

--- app.js
+++ app.js
@@ -73,25 +73,37 @@
 // 3rd Part: Feeling Loopy!
 //////////////////////////
 
-const data =
-  "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26";
+// const data =
+//   "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19,32\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26";
 
-const separatedLine = data.split("\n");
+// const separatedLine = data.split("\n");
 
-for (let i = 0; i < separatedLine.length; i++) {
-  let word = separatedLine[i];
-  let rows = word.split(",");
-  if (rows.length === 4) {
-    console.log(rows[0], rows[1], rows[2], rows[3]);
-  }
-}
+// for (let i = 0; i < separatedLine.length; i++) {
+//   let word = separatedLine[i];
+//   let cells = word.split(",");
+//   if (cells.length === 4) {
+//     console.log(cells[0], cells[1], cells[2], cells[3]);
+//   }
+// }
 
 // // with Try and catch method!
+// // i put more cells in 3rd row to test my error handling
 
-// for(let i = 0; i < separatedLine.length; i++) {
-//     try {
-//         let cells = separatedLine[i]
-//     } catch (error) {
+const data2 =
+  "Index,Mass (kg),Spring 1 (m),Spring 2 (m)\n1,0.00,0.050,0.050\n2,0.49,0.066,0.066\n3,0.98,0.087,0.080,0.45\n4,1.47,0.116,0.108\n5,1.96,0.142,0.138\n6,2.45,0.166,0.158\n7,2.94,0.193,0.174\n8,3.43,0.204,0.192\n9,3.92,0.226,0.205\n10,4.41,0.238,0.232";
 
-//     }
-// }
+const separatedLine2 = data2.split("\n");
+
+for (let i = 0; i < separatedLine2.length; i++) {
+  try {
+    let word = separatedLine2[i];
+    let rows = word.split(",");
+    if (rows.length === 4) {
+      console.log(rows[0], rows[1], rows[2], rows[3]);
+    } else {
+      throw new Error(`Number of cells not match ${i + 1}`);
+    }
+  } catch (err) {
+    console.log(err);
+  }
+}
